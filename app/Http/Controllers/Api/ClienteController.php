9921820074cb6a961e7225ee71c07708f4b07dc1
Lucas Corrêa <lc_correa@outlook.com>
2020-04-16 11:05:32 -0300

    Inserindo imagens no crud

--- app/Http/Controllers/Api/ClienteController.php
+++ app/Http/Controllers/Api/ClienteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cliente;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class ClienteController extends Controller
 {
@@ -21,6 +22,17 @@ class ClienteController extends Controller
     public function store(){
         $this->validate($this->Request, $this->Cliente->rules());
         $dataForm = $this->Request->all();
+        if($this->Request->hasFile('imagem') && $this->Request->file('imagem')->isValid()){
+            $extension = $this->Request->imagem->extension();
+            $name = uniqid(date('His'));            
+            $nameFile = "{$name}.{$extension}";
+            $upload = Image::make($dataForm['imagem'])->resize(177,236)->save(storage_path("app/public/clientes/$nameFile", 70));
+            if (!$upload){
+                return response()->json(['error'=>'Falha ao Fazer Upload'], 500);
+            } else {
+                $dataForm['imagem'] = $nameFile;
+            }
+        }
         $data = $this->Cliente->create($dataForm);
         return response()->json($data, 201);
     }
